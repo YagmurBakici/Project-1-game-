7dec966a11c7fdee70484466942b597b34ac1e33
Yagmur <bakiciyagmur@gmail.com>
2019-04-04 15:59:55 +0200

    updated

--- pingypongy.js
+++ pingypongy.js
@@ -7,10 +7,7 @@ canvas.height = canvas.scrollHeight;
 var ctx = canvas.getContext("2d");
 ctx.font = "20px Monospace";
 
-var startBtn = StartButton();
-//SCORE
-var score1 = 0;
-var score2 = 0;
+startBtn = {};
 
 function Ball() {
   this.isActive = true;
@@ -38,10 +35,10 @@ Ball.prototype.setActive = function() {
 Ball.prototype.reset = function() {
   console.log("reset");
   // ball = new Ball();
+  ball = new Ball();
   // window.cancelAnimationFrame(intervalId);
   setTimeout(function() {
     // isActive = true;
-    ball = new Ball();
     // intervalId = window.requestAnimationFrame(draw);
   }, 1000); // wait 1 second
   //ball.draw();
@@ -60,9 +57,12 @@ Ball.prototype.checkBorderRebounce = function() {
 //right and left getting out off
 Ball.prototype.checkBorderExit = function() {
   if (ball.x > canvas.width + 2 * ball.ballRadius) {
+    console.log("riıght sıde");
+
     return true;
   }
   if (ball.x < -2 * ball.ballRadius) {
+    console.log("left sıde");
     return true;
   }
   return false;
@@ -75,8 +75,7 @@ function Paddle(color, x, y, score) {
   this.y = y;
   this.h = 100;
   this.w = 20;
-  this.score1 = 0;
-  this.score2 = 0;
+  this.score = 0;
 }
 
 Paddle.prototype.draw = function() {
@@ -87,8 +86,8 @@ Paddle.prototype.draw = function() {
 
 var ball = new Ball();
 //creation of Player1-Player2
-var player1 = new Paddle("greenyellow", 10, canvas.height / 2, score1); // params are color, x pos, y pos for each new Paddle
-var player2 = new Paddle("greenyellow", 755, canvas.height / 2, score2);
+var player1 = new Paddle("greenyellow", 10, canvas.height / 2, 0); // params are color, x pos, y pos for each new Paddle
+var player2 = new Paddle("greenyellow", 755, canvas.height / 2, 0);
 
 //                    ------------------- PLAYER ONE -------------------
 
@@ -100,10 +99,14 @@ Paddle.prototype.hitP1 = function() {
     ball.y + ball.dy > player1.y
   ) {
     ball.dx = -ball.dx; // hit the ball on the opposite way
+    return true;
   }
+  return false;
+  // function + if ==>you need to return something bu ikisi olunca
 };
 
 //                   ------------------- PLAYER TWO -------------------
+
 Paddle.prototype.hitP2 = function() {
   if (
     ball.x < player2.x + player2.w &&
@@ -112,27 +115,27 @@ Paddle.prototype.hitP2 = function() {
     ball.y + ball.dy > player2.y
   ) {
     ball.dx = -ball.dx;
+    return true;
   }
+  return false;
 };
 
 function displayScore() {
   ctx.fillText(
-    " P1: " +
-      score1 +
-      "                                                  P2: " +
-      score2,
-    10,
+    "   P1: " +
+      player1.score +
+      "                                                P2: " +
+      player2.score,
+    0,
     20
   );
 }
 
 function keyDownHandler(e) {
-  //console.log(e);
-  //ctx.clearRect(this.x, this.y, this.w, this.h);
   if (e.key === "s") {
-    // console.log("uppppp");
     player1.y -= 60; // speed of the paddle
   } else if (e.key === "z") {
+    //key up
     player1.y += 60;
   } else if (e.key === "ArrowUp") {
     player2.y -= 60;
@@ -141,28 +144,25 @@ function keyDownHandler(e) {
   }
 }
 
-function StartButton() {
-  return {
-    w: 100,
-    h: 50,
-    x: canvas.width / 2 - 50,
-    y: canvas.height / 2 - 25,
+// Start Button object
+startBtn = {
+  w: 100,
+  h: 50,
+  x: canvas.width / 2 - 50,
+  y: canvas.height / 2 - 25
+};
 
-    draw: function() {
-      ctx.strokeStyle = "greenyellow";
-      ctx.lineWidth = "2";
-      ctx.strokeRect(this.x, this.y, this.w, this.h);
+startBtn.draw = function() {
+  ctx.strokeStyle = "greenyellow";
+  ctx.lineWidth = "10";
+  ctx.strokeRect(this.x, this.y, this.w, this.h);
 
-      ctx.font = "18px Monospace";
-      ctx.textAlign = "center";
-      ctx.textBaseline = "middle";
-      ctx.fillStlye = "Green";
-      ctx.fillText("Start", W / 2, H / 2);
-    }
-  };
-}
-
-function checkPlaygroundExit() {}
+  ctx.font = "18px monospace";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  ctx.fillStlye = "Green";
+  ctx.fillText("Start", canvas.width / 2, canvas.height / 2);
+};
 
 //                          ---------------------DRAW FUNCTIONS------------------
 
@@ -172,84 +172,78 @@ function draw(now) {
   player1.draw();
   player2.draw();
   ball.draw();
+  // startBtn.draw();
+  player1.hitP1();
+  player2.hitP2();
 
-  if (player1.hitP1() && ball.checkBorderExit()) {
-    score1++;
-  }
-  //   ball.reset();
-  //   ball.dy = -4; //horizantal direction
-  //   ball.dx = -4; //vertical direction
-  // }
-
-  if (player2.hitP2() && ball.checkBorderExit()) {
-    score2++;
-  }
-
-  // if (now % 60 === 1) {
   ball.x += ball.dx;
   ball.y += ball.dy;
   // }
 
   if (ball.checkBorderExit()) {
     //if ball exits from left right axes reset the ball
+    const winner = ball.x < 0 ? "player2" : "player1";
+    console.log("someone wıns", winner);
+    // console.log();
+
     ball.reset();
   }
 
   ball.checkBorderRebounce();
   window.requestAnimationFrame(draw);
-
-  //pLAYER 1
-  // if (
-  //   ball.y > canvas.height + ball.ballRadius ||
-  //   ball.y < canvas.height + ball.ballRadius
-  // ) {
-  // }
-  // if (ball.x - ball.ballRadius < player1.x + player1.w) {
-  //   console.log("It works??");
-  //   isActive = false;
-  // }
-  // if (
-  //   ball.x < player1.x + player1.w &&
-  //   ball.x + ball.dx > player1.x &&
-  //   ball.y < player1.y + player1.h &&
-  //   ball.y + ball.dy > player1.y
-  // ) {
-  //   ball.dx = -ball.dx;
-  //   // isActive = true;
-  // }
-  // /// ------------ PLAYER TWO -------------
-
-  // if (
-  //   ball.x < player2.x + player2.w &&
-  //   ball.x + ball.dx > player2.x &&
-  //   ball.y < player2.y + player2.h &&
-  //   ball.y + ball.dy > player2.y
-  // ) {
-  //   ball.dx = -ball.dx;
-  //   score2++;
-  // }
-
-  // // ----------------------------------- BOUNCING OFF EDGES ----------------------------------
-
-  // //-----------When it hits the left and right
-
-  // if (ball.x + ball.dx > canvas.width - ball.ballRadius) {
-  //   // console.log("passing the X axes");
-
-  //   ball.dx = -ball.dx;
-  //   isActive = false;
-  // } else if (ball.x < 0) {
-  //   ball.dx = +ball.dx;
-  //   isActive = false;
-  // }
-  // ///------------------When it hits the bottom and top---------------
-  // else if (
-  //   ball.y + ball.dy > canvas.height - ball.ballRadius ||
-  //   ball.y + ball.dy < ball.ballRadius
-  // ) {
-  //   ball.dy = -ball.dy;
-  // }
 }
+
+//pLAYER 1
+// if (
+//   ball.y > canvas.height + ball.ballRadius ||
+//   ball.y < canvas.height + ball.ballRadius
+// ) {
+// }
+// if (ball.x - ball.ballRadius < player1.x + player1.w) {
+//   console.log("It works??");
+//   isActive = false;
+// }
+// if (
+//   ball.x < player1.x + player1.w &&
+//   ball.x + ball.dx > player1.x &&
+//   ball.y < player1.y + player1.h &&
+//   ball.y + ball.dy > player1.y
+// ) {
+//   ball.dx = -ball.dx;
+//   // isActive = true;
+// }
+// /// ------------ PLAYER TWO -------------
+
+// if (
+//   ball.x < player2.x + player2.w &&
+//   ball.x + ball.dx > player2.x &&
+//   ball.y < player2.y + player2.h &&
+//   ball.y + ball.dy > player2.y
+// ) {
+//   ball.dx = -ball.dx;
+//   score2++;
+// }
+
+// // ----------------------------------- BOUNCING OFF EDGES ----------------------------------
+
+// //-----------When it hits the left and right
+
+// if (ball.x + ball.dx > canvas.width - ball.ballRadius) {
+//   // console.log("passing the X axes");
+
+//   ball.dx = -ball.dx;
+//   isActive = false;
+// } else if (ball.x < 0) {
+//   ball.dx = +ball.dx;
+//   isActive = false;
+// }
+// ///------------------When it hits the bottom and top---------------
+// else if (
+//   ball.y + ball.dy > canvas.height - ball.ballRadius ||
+//   ball.y + ball.dy < ball.ballRadius
+// ) {
+//   ball.dy = -ball.dy;
+// }
 
 document.onkeydown = keyDownHandler; //action when the key is pressed
 
